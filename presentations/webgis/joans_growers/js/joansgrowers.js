5de28394834a924c32061b606f7e5f46c637a6d1
tylerdardis <tylerdardis@icloud.com>
2021-03-09 21:56:25 -0600

    update

---
+++ presentations/webgis/joans_growers/js/joansgrowers.js
@@ -0,0 +1,16 @@
+function schools() {
+        var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([-93.166, 44.962]),
+          zoom: 10
+        })
+      });
+
+}
+
